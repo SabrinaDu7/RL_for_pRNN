8e389522ae9c0e95f2b24c9bec6ac151f8b0fdc8
SabrinaDu <sabrina.du1972@gmail.com>
2025-10-21 11:44:15 -0400

    Added comments to understand code in the config files

--- RLutils/__init__.py
+++ RLutils/__init__.py
@@ -1,15 +1,130 @@
+# Agent classes and functions
 from .agent import (
     Agent,
-    ActorCriticAgent
+    ActorCriticAgent,
 )
-from .env import *
-from .format import *
-from .other import *
-from .storage import *
-from .pc import *
+
+# Algorithm classes
+from .algo import (
+    compare_trajs,
+    PredictivePPOAlgo,
+    thetaPPOalgo,
+    SingleThetaPPOalgo,
+)
+
+# Analysis classes and functions
+from .analysis import (
+    mutual_info_policy,
+    plot_heatmaps,
+    EnvironmentFeaturesAnalysis,
+    OnPolicyAnalysis,
+)
+
+# Environment functions and classes
+from .env import (
+    episode_video_trigger,
+    make_env,
+    ResetWrapper,
+    HDObsWrapper,
+)
+
+# Format/preprocessing functions and classes
+from .format import (
+    get_obss_preprocessor,
+    preprocess_images,
+    preprocess_int,
+    preprocess_texts,
+    Vocabulary,
+)
+
+# Model classes and functions
+from .model import (
+    init_params,
+    RecACModel,
+    ACModel,
+    ACModelSR,
+    ACModelTheta,
+    ACModelThetaShared,
+    ACModelThetaSingle,
+)
+
+# Other utility functions and constants
+from .other import (
+    DEVICE,
+    seed,
+    synthesize,
+)
+
+# Place cells class
+from .pc import (
+    FakePlaceCells,
+)
+
+# Storage functions
+from .storage import (
+    create_folders_if_necessary,
+    get_storage_dir,
+    get_model_dir,
+    get_video_dir,
+    get_tmp_dir,
+    get_tmp_model_dir,
+    get_status_path,
+    get_status,
+    get_pN,
+    save_status,
+    save_analysis_of_agent_behav,
+)
 
 
 __all__ = [
+    # Agent
     "Agent",
     "ActorCriticAgent",
+    # Algo
+    "compare_trajs",
+    "PredictivePPOAlgo",
+    "thetaPPOalgo",
+    "SingleThetaPPOalgo",
+    # Analysis
+    "mutual_info_policy",
+    "plot_heatmaps",
+    "EnvironmentFeaturesAnalysis",
+    "OnPolicyAnalysis",
+    # Env
+    "episode_video_trigger",
+    "make_env",
+    "ResetWrapper",
+    "HDObsWrapper",
+    # Format
+    "get_obss_preprocessor",
+    "preprocess_images",
+    "preprocess_int",
+    "preprocess_texts",
+    "Vocabulary",
+    # Model
+    "init_params",
+    "RecACModel",
+    "ACModel",
+    "ACModelSR",
+    "ACModelTheta",
+    "ACModelThetaShared",
+    "ACModelThetaSingle",
+    # Other
+    "DEVICE",
+    "seed",
+    "synthesize",
+    # PC
+    "FakePlaceCells",
+    # Storage
+    "create_folders_if_necessary",
+    "get_storage_dir",
+    "get_model_dir",
+    "get_video_dir",
+    "get_tmp_dir",
+    "get_tmp_model_dir",
+    "get_status_path",
+    "get_status",
+    "get_pN",
+    "save_status",
+    "save_analysis_of_agent_behav",
 ]
